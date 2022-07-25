e6cc8fbc572404c5abc8745ef7e97795226aa3af
Jack McKelvey <71670664+McKelvey-Jack@users.noreply.github.com>
2022-07-25 22:33:18 +0100

    add seeders and factories

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Product;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Name of the factory's model
+     */
+    protected $model = Product::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => 'Test Shoe',
+        ];
+    }
+}
